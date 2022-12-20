ad2fe864a0c432c4089c38a082b996ede36180d5
Vladimir Balakin <vladimirbalakin933@gmail.com>
2022-12-21 00:58:07 +0300

    Теперь, если введены некорректные данные, программа завершится с exit code 1.

--- src/CreditDataWrite.java
+++ src/CreditDataWrite.java
@@ -1,24 +1,21 @@
 public class CreditDataWrite {
-    static void writeData(CreditData credit, String[] inputData)  {
-        try {
-            if (Double.parseDouble(inputData[0]) > 0
-                    && Double.parseDouble(inputData[1]) > 0
-                    && Double.parseDouble(inputData[2]) >= 0
-                    && (inputData[3].trim().equals("business") || inputData[3].trim().equals("human"))
-                    && !(inputData[3].trim().equals("human")
-                        && Double.parseDouble(inputData[0]) * (1 + Double.parseDouble(inputData[2]) / 100)  > 12 * Double.parseDouble(inputData[1])
-                        && ((Double.parseDouble(inputData[0]) * (1 + Double.parseDouble(inputData[2]) / 100) -
-                            12 * Double.parseDouble(inputData[1])) * (1 + Double.parseDouble(inputData[2]) / 100)) >= Double.parseDouble(inputData[0]))
-                    && !(inputData[3].trim().equals("business") && Double.parseDouble(inputData[0]) > 12 * Double.parseDouble(inputData[1])
-                        && ((Double.parseDouble(inputData[0]) - 12 * Double.parseDouble(inputData[1])) *
-                            (1 + Double.parseDouble(inputData[2]) / 100)) >= Double.parseDouble(inputData[0]))) {
-                credit.credit = Double.parseDouble(inputData[0]);
-                credit.pay = Double.parseDouble(inputData[1]);
-                credit.percent = Double.parseDouble(inputData[2]);
-                credit.type = inputData[3].trim();
-            }
-        } catch (Exception ex) {
-          //  System.out.println(ex.getMessage());
+    static void writeData(CreditData credit, String[] inputData) {
+
+        if (Double.parseDouble(inputData[0]) > 0
+                && Double.parseDouble(inputData[1]) > 0
+                && Double.parseDouble(inputData[2]) >= 0
+                && (inputData[3].trim().equals("business") || inputData[3].trim().equals("human"))
+                && !(inputData[3].trim().equals("human")
+                && Double.parseDouble(inputData[0]) * (1 + Double.parseDouble(inputData[2]) / 100) > 12 * Double.parseDouble(inputData[1])
+                && ((Double.parseDouble(inputData[0]) * (1 + Double.parseDouble(inputData[2]) / 100) -
+                12 * Double.parseDouble(inputData[1])) * (1 + Double.parseDouble(inputData[2]) / 100)) >= Double.parseDouble(inputData[0]))
+                && !(inputData[3].trim().equals("business") && Double.parseDouble(inputData[0]) > 12 * Double.parseDouble(inputData[1])
+                && ((Double.parseDouble(inputData[0]) - 12 * Double.parseDouble(inputData[1])) *
+                (1 + Double.parseDouble(inputData[2]) / 100)) >= Double.parseDouble(inputData[0]))) {
+            credit.credit = Double.parseDouble(inputData[0]);
+            credit.pay = Double.parseDouble(inputData[1]);
+            credit.percent = Double.parseDouble(inputData[2]);
+            credit.type = inputData[3].trim();
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditData credit = new CreditData();
-        try {
-            CreditDataWrite.writeData(credit, CreditInput.inputData());
-            System.out.println(CreditCalculator.calculateCredit(credit));
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        CreditDataWrite.writeData(credit, CreditInput.inputData());
+        System.out.println(CreditCalculator.calculateCredit(credit));
     }
 }
